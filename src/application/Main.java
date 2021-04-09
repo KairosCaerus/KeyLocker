6d991dbe5ab235bec3c1c3b9f5314552c5a59cc7
Christopher Louie <christopherlouie16@gmail.com>
2021-04-08 18:51:35 -0700

    added simple login check, username=username and password=password

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,21 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
@@ -29,10 +33,9 @@ public class Main extends Application {
 			title.setFont(Font.font("Arial", FontWeight.BOLD, 40));
 			logoTitleBox.getChildren().add(title);
 			logoTitleBox.setAlignment(Pos.CENTER);
-
 			
 			// Creating username label and text field
-			Label usernameLbl = new Label("username: ");
+			Label usernameLbl = new Label("Username: ");
 			usernameLbl.setFont(Font.font("Arial", FontWeight.SEMI_BOLD, 20));
 			
 			TextField usernameInput = new TextField();
@@ -40,10 +43,10 @@ public class Main extends Application {
 			usernameInput.setPrefHeight(30);
 			
 			// Creating password label and text field
-			Label passwordLbl = new Label("password: ");
+			Label passwordLbl = new Label("Password: ");
 			passwordLbl.setFont(Font.font("Arial", FontWeight.SEMI_BOLD, 20));
 			
-			TextField passwordInput = new TextField();
+			PasswordField passwordInput = new PasswordField();
 			passwordInput.setPrefWidth(200);
 			passwordInput.setPrefHeight(30);
 			
@@ -56,13 +59,28 @@ public class Main extends Application {
 			textFields.setHgap(3);
 			textFields.setAlignment(Pos.CENTER);
 			
-			// Adding Login button
+			// Adding Login button and Input label
+			Label inputLbl = new Label("");
 			Button loginBtn = new Button("Login");
+			inputLbl.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 			loginBtn.setFont(Font.font("Arial", 20));
+			loginBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent> () {
+				@Override
+				public void handle(MouseEvent event) {
+					if(!usernameInput.getText().equals("username") || !passwordInput.getText().equals("password")) {
+						inputLbl.setText("Username or Password is incorrect");
+						inputLbl.setTextFill(Color.RED);
+					} else {
+						inputLbl.setText("Login Successful");
+						inputLbl.setTextFill(Color.LIGHTGREEN);
+					}
+
+				}
+			});
 			
-			// Adding nodes to centerVBox and setting its alignment to center
+			// Adding nodes to VBox and setting its alignment to center
 			VBox centerVBox = new VBox();
-			centerVBox.getChildren().addAll(logoTitleBox, textFields, loginBtn);
+			centerVBox.getChildren().addAll(logoTitleBox, textFields, loginBtn, inputLbl);
 			centerVBox.setAlignment(Pos.CENTER);
 			centerVBox.setSpacing(10);
 
