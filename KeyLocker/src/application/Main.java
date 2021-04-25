50a890d481106428baa6be0a1416c481f0b42190
Johann Kwon <johannkwon01@gmail.com>
2021-04-24 19:00:58 -0700

    Added CreatorView.java, AccountView.java, changed Main.java

---
+++ KeyLocker/src/application/Main.java
@@ -0,0 +1,139 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import lib.Generation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import views.GeneratorView;
+import views.LoginView;
+import views.AccountView;
+import views.CreatorView;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Initialize loginView, generatorView, accountView, creatorView
+			LoginView loginView = new LoginView();
+			GeneratorView generatorView = new GeneratorView();
+			AccountView accountView = new AccountView();
+			CreatorView creatorView = new CreatorView();
+			
+			// Initialize roots for scene changes
+			BorderPane root = loginView.getPaneRoot();
+			BorderPane accountRoot = accountView.getPaneRoot();
+			
+			// Initialize buttons for event handling
+			Button loginBtn = loginView.getLoginBtn();
+			Button generatorReturnBtn = generatorView.getReturnBtn();
+			Button passwordGenBtn = accountView.getGeneratorBtn();
+			Button logoutBtn = accountView.getLogoutBtn();
+			Button accountCrtBtn = accountView.getCreatorBtn();
+			Button creatorReturnBtn = creatorView.getReturnBtn();
+			Button createAccountBtn = creatorView.getCreateBtn();
+			
+			// Setting size of window
+			Scene scene = new Scene(root, 800, 600);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Key Locker");
+			primaryStage.show();
+
+			// Handles pressing enter to login
+			root.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					// If the user sends the correct login, switch roots.
+					if (event.getCode() == KeyCode.ENTER && loginView.checkLogin()) {
+						scene.setRoot(accountRoot);
+					}
+				}
+			});
+
+			// Handles clicking of the login button
+			loginBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					// If the user sends the correct login, switch roots.
+					if (loginView.checkLogin()) {
+						scene.setRoot(accountRoot);
+					}
+				}
+			});
+			
+			// Handles clicking of the Create new account button
+			accountCrtBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					// Switch to Account Creator root when button is clicked
+					scene.setRoot(creatorView.getPaneRoot());
+				}
+			});
+			
+			// Handles clicking of the Password Generator button
+			passwordGenBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					// Switches to Password Generator root when button is clicked
+					scene.setRoot(generatorView.getPaneRoot());
+				}
+			});
+			
+			// Handles clicking back to main button (in Password Generator)
+			generatorReturnBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					// Switches back to main root when button is clicked
+					scene.setRoot(accountRoot);
+				}
+			});
+			
+
+			// Handles clicking Create Account button (in Account Creator)
+			createAccountBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					if(creatorView.fieldEmpty()) {
+						// If the required fields aren't empty, switch to main root
+						scene.setRoot(accountRoot);
+					}
+				}
+			});
+			
+			// Handles clicking back to main button (in Account Creator)
+			creatorReturnBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					// Switches back to main root when button is clicked
+					scene.setRoot(accountRoot);
+				}
+			});
+			
+			// TODO: exit when pressing logout
+			// Handles clicking of the Back to Main button
+			logoutBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					// Exit application when button is clicked
+					scene.setRoot(accountRoot);
+				}
+			});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ KeyLocker/src/views/AccountView.java
@@ -0,0 +1,95 @@
+package views;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class AccountView {
+	
+	private BorderPane accountRoot;
+	private Label inputLbl;
+	private Button creatorBtn;
+	private Button generatorBtn;
+	private Button logoutBtn;
+
+	public AccountView() {
+		accountRoot = new BorderPane();
+		
+		accountRoot.setPadding(new Insets(35, 10, 20, 20));
+		
+		// TODO: Add lock image 
+		// Creating title & logo box
+		HBox titleBox = new HBox();
+		Label title = new Label("Account Summary");
+		title.setFont(Font.font("Arial", FontWeight.BOLD, 40));
+		titleBox.getChildren().add(title);
+		titleBox.setAlignment(Pos.CENTER);
+		accountRoot.setTop(titleBox);
+		
+		// Adding Create New Account button, Password Generator button, Sign Out
+		inputLbl = new Label("");
+		inputLbl.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		
+		creatorBtn = new Button("Create New Account");
+		creatorBtn.setFont(Font.font("Arial", 20));
+		
+		generatorBtn = new Button("Password Generator");
+		generatorBtn.setFont(Font.font("Arial", 20));
+		
+		logoutBtn = new Button("Sign Out");
+		logoutBtn.setFont(Font.font("Arial", 20));
+		
+		// Adding nodes to VBox and setting its alignment to center
+		VBox sidebarVBox = new VBox();
+		sidebarVBox.getChildren().addAll(creatorBtn, generatorBtn, logoutBtn, inputLbl);
+		sidebarVBox.setAlignment(Pos.CENTER);
+		sidebarVBox.setSpacing(10);
+		
+		// Adding nodes to root and setting background color
+		accountRoot.setLeft(sidebarVBox);
+		accountRoot.setStyle("-fx-background-color: cornflowerblue");
+	}
+	
+	/**
+	 * Return root of the class in the form of a BorderPane object 
+	 * 
+	 * @return root of AccountView
+	 */
+	
+	public BorderPane getPaneRoot() {
+		return accountRoot;
+	}
+	
+	/**
+	 * Returns Button object for sign out
+	 * 
+	 * @return logoutBtn
+	 */
+	public Button getLogoutBtn() {
+		return logoutBtn;
+	}
+	
+	/**
+	 * Returns Button object for generator
+	 * 
+	 * @return generatorBtn
+	 */
+	public Button getGeneratorBtn() {
+		return generatorBtn;
+	}
+	
+	/**
+	 * Returns Button object for creator
+	 * 
+	 * @return creatorBtn
+	 */
+	public Button getCreatorBtn() {
+		return creatorBtn;
+	}
+}
